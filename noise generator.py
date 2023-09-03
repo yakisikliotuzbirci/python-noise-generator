112ffa2d8c1f9b055c3a713d1e0141d19f5671d9
SametSever <139774058+SametSever@users.noreply.github.com>
2023-09-03 22:33:17 +0300

    Add files via upload

---
+++ noise generator.py
@@ -0,0 +1,45 @@
+from random import randint
+from PIL import Image, ImageDraw
+import sys
+
+def main():
+    w = int(input("width : "))
+    h = int(input("height : "))
+
+    resolution = w, h
+
+    x, y = 0, 0
+
+    image = Image.new("RGB", (resolution[0], resolution[1]), "white")
+    draw = ImageDraw.Draw(image)
+
+    while True:
+        x = x + 1
+        
+        if x == resolution[0]:
+            y = y + 1
+            x = 0
+
+        r = randint(0, 5)
+
+        if r == 0:
+            color = "#000000"
+        elif r == 1:
+            color = "#ffffff"
+        elif r == 2:
+            color = "#b1b1b1"
+        elif r == 3:
+            color = "#fbfbfb"
+        elif r == 4:
+            color = "#dbdbdb"
+        elif r == 5:
+            color = "#1f1f1f"
+
+        draw.line([(x, y), (x + 1, y + 1)], fill=color)
+
+        if y == resolution[1]:
+            image.save(r"noise.png")
+            sys.exit()
+
+if __name__ == "__main__":
+    main()
